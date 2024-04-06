88c44c2ecbc992f16088d2ed0301ba454ebb000b
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-04-06 03:17:46 +0300

    ana sayfa hazırlanıyor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import { Button } from "@/components/ui/button";
-
+import {UserButton} from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
