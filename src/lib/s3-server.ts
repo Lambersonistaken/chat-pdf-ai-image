cabea9b94c1410cf2d50435f45b052bd0098b924
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-04-17 02:06:27 +0300

    fixlendi s3-server

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -2,30 +2,30 @@ import AWS from 'aws-sdk'
 import fs from 'fs'
 export async function downloadFromS3(file_key: string) {
 
-try {
-    AWS.config.update({
-        accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
-        secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
+    try {
+        AWS.config.update({
+            accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
+            secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY,
 
-    });
-    const s3 = new AWS.S3({
-        params: {
-            Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
-        },
-        region : 'eu-north-1'
-    });
+        });
+        const s3 = new AWS.S3({
+            params: {
+                Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
+            },
+            region: 'eu-north-1'
+        });
 
-    const params = {
-        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
-        Key: file_key,
-    };
+        const params = {
+            Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+            Key: file_key,
+        };
 
-    const obj = await s3.getObject(params).promise()
-    const file_name = '/tmp/pdf-${Date.now()}.pdf'  //burda -$ ile sorun yaşadım
-    fs.writeFileSync(file_name, obj.Body as Buffer)
-    return file_name
-} catch (error) {
-    console.error(error)
-    return null
-}
+        const obj = await s3.getObject(params).promise()
+        const file_name = `/tmp/pdf-${Date.now()}.pdf`  //burda -$ ile sorun yaşadım ----fixlendi
+        fs.writeFileSync(file_name, obj.Body as Buffer)
+        return file_name
+    } catch (error) {
+        console.error(error)
+        return null
+    }
 }
